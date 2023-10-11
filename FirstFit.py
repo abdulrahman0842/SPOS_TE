b390fec39e2b0b12cefcfc544d367188a15145e3
Abdulrahman <94384418+abdulrahman0842@users.noreply.github.com>
2023-10-11 22:21:43 +0530

    First Fit Strategy (Memory Allocation Strategy)

---
+++ FirstFit.py
@@ -0,0 +1,25 @@
+blockSize = [300,50,200,350,70]
+processSize = [200,47,212,426,10]
+m = len(processSize)
+n = len(blockSize)
+blockNum = [-1]*n
+
+
+for i in range(m):
+    for j in range(n):
+        list=[-1]*n
+        blockSize[j] = blockSize[j]-processSize[i]
+        index = blockSize.index(min(blockSize))
+print(blockSize)
+
+
+for i in range(m):
+    for j in range(n):
+        if blockSize[j]>=processSize[i]:
+            afterFit = blockSize[j]-processSize[i]
+            blockSize[j]= afterFit
+            blockNum[j]= j+1
+            break
+        if processSize[i]>blockSize[j]:
+            blockNum[j]='not allocated'
+    print(i,'\t',processSize[i],'\t',blockNum[j])
