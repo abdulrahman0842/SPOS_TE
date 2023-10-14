76f093f816449dc54237391d114b493c57952cdd
Abdulrahman <94384418+abdulrahman0842@users.noreply.github.com>
2023-10-14 09:05:15 +0530

    Update FCFS.py

--- FCFS.py
+++ FCFS.py
@@ -1,18 +1,27 @@
-processes = [['p1',0,2],['p2',1,2],['p3',5,3],['p4',6,4]]
-ct = [0,0,0,0]
-tat = [0]*len(ct)
-wt = [0]*len(ct)
-for i in range(4):
-    ct[i] = processes[i][2]+ct[i-1]
-    tat[i] = ct[i]-processes[i][1]
-    wt[i] = tat[i]-processes[i][2]
+processes=[['p1',1,6],['p2',4,4],['p3',2,3],['p4',5,6]]
+ct=[0]*len(processes)
+tat=[0]*len(processes)
+wt=[0]*len(processes)
+
+
+for i in range(len(processes)):
+    min_idx = i
+    for j in range(i + 1, len(processes)):
+        if processes[min_idx][1] > processes[j][1]:
+            min_idx = j
+    processes[i], processes[min_idx] = processes[min_idx], processes[i]
+
+for i in range(len(processes)):
+    empty=0
+    if processes[i][1]>ct[i-1]:
+        empty=1
+    ct[i]=processes[i][2]+ct[i-1]+empty
+    tat[i]=ct[i]-processes[i][1]
+    wt[i]=tat[i]-processes[i][2]
+
 print('Process','AT\t','BT\t','CT\t','TAT\t','WT\t')
-for j in range(4):
-    print(processes[j][0],'\t',processes[j][1],'\t',processes[j][2],'\t',ct[j],'\t',tat[j],'\t',wt[j])
-    
-    
-# process = ['p1','p2','p3','p4']
-# at = [0,1,4,5]
-# bt = [2,2,3,4]
-# at = [0,1,2,3]
-# bt = [5,3,8,6]
+for j in range(len(processes)):
+    print(processes[j][0],'\t',processes[j][1],'\t',processes[j][2],'\t',ct[j],'\t',tat[j],'\t',wt[j],'\n')
+
+print('TAT:',sum(tat)/len(processes))
+print('WT:',sum(wt)/len(processes))
